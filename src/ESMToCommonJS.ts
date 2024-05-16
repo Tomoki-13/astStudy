1eb35724dd0fa6b55a1abc73c40c6410e56493b1
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-05-16 15:30:33 +0900

    fix/optionetc

--- src/ESMToCommonJS.ts
+++ src/ESMToCommonJS.ts
@@ -5,8 +5,8 @@ import traverse from "@babel/traverse";
 
 (async () => {
 	try {
-		const libName: string  = process.argv[2];
-		const filePath:string = './acornAstSample.ts';
+		const searchWord: string  = process.argv[2];
+		const filePath:string = '../codesample/samplecode.js';
 		const fileContent: string = await fsPromises.readFile(filePath, "utf8");
 
 		const commonJsResult = babel.transformSync(fileContent, {
@@ -17,7 +17,8 @@ import traverse from "@babel/traverse";
 		if (commonJsResult !== null && commonJsResult.code !== null && commonJsResult.code !== undefined) {
 			console.log(commonJsResult.code);
 			const parsed = parser.parse(commonJsResult.code, {
-				sourceType: "script", 
+				sourceType: "unambiguous", 
+				plugins: ["typescript"]
 			});
 			const codes: string[] = [];
 
@@ -25,24 +26,24 @@ import traverse from "@babel/traverse";
 				VariableDeclaration(path: any) {
 					const node = path.node;
 					for (const declaration of node.declarations) {
-						if (declaration.id.type === 'Identifier' && declaration.id.name.includes(libName)) {
+						if (declaration.id.type === 'Identifier' && declaration.id.name.includes(searchWord)) {
 						const code = fileContent.substring(node.start, node.end);
 						codes.push(code);
 						}
 					}
 				},
 				CallExpression(path: any) {
-					const node = path.node;
-					if (node.callee.type === 'Identifier' && node.callee.name === libName) {
-						const code: string = fileContent.substring(node.start, node.end);
-						codes.push(code);
-					} else if (node.callee.type === 'MemberExpression') {
-						if (node.callee.object && node.callee.object.type === 'Identifier' && node.callee.object.name === libName) {
-						const code: string = fileContent.substring(node.start, node.end);
-						codes.push(code);
-						}
-					}
-				}
+                    const node = path.node;
+                    //関数の呼び出しを見つける
+                    if (node.callee.type === 'Identifier' && node.callee.name.includes(searchWord)) {
+                        //UUID関数の呼び出しを見つけたら、そのノードを文字列に変換して保存
+                        const code: string = fileContent.substring(node.start, node.end);
+                        codes.push(code);
+                    } else if(node.callee.type === 'MemberExpression'&&node.callee.object.type === 'Identifier'&& node.callee.object.name.includes(searchWord)){
+                        const code: string = fileContent.substring(node.start, node.end);
+                        codes.push(code);
+                    }
+                }
 			});
 
 			console.log(codes);
