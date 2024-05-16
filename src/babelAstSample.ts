4d89ad0d5a189b458250b8d6d427936903f2aba1
Tomoki-13 <s266016@wakayama-u.ac.jp>
2024-05-16 15:23:28 +0900

    fix/bug

--- src/babelAstSample.ts
+++ src/babelAstSample.ts
@@ -5,17 +5,17 @@ import traverse from "@babel/traverse";
 (async () => {
     let codes:string[] = [];
     try {
-        const libName: string  = process.argv[2];
+        const searchWord: string  = process.argv[2];
         //ファイルの内容を取得
-        const filePath:string = './acornAstSample.js';
+        const filePath:string = '../codesample/samplecode.js';
         if (filePath.endsWith('.js') || filePath.endsWith('.ts')) {
             const fileContent: string = await fsPromises.readFile(filePath, 'utf8');
-            const parsed = parser.parse(fileContent, {sourceType: 'script', plugins: ["typescript"] });
+            const parsed = parser.parse(fileContent, {sourceType: 'unambiguous', plugins: ["typescript"] });
             traverse(parsed, {
                 VariableDeclaration(path: any) {
                     const node = path.node;
                     for (const declaration of node.declarations) {
-                        if (declaration.id.type === 'Identifier' && declaration.id.name.includes(libName)) {
+                        if (declaration.id.type === 'Identifier' && declaration.id.name.includes(searchWord)) {
                             const code = fileContent.substring(node.start, node.end);
                             codes.push(code);
                         }
@@ -24,15 +24,13 @@ import traverse from "@babel/traverse";
                 CallExpression(path: any) {
                     const node = path.node;
                     //関数の呼び出しを見つける
-                    if (node.callee.type === 'Identifier' && node.callee.name.includes(libName)) {
+                    if (node.callee.type === 'Identifier' && node.callee.name.includes(searchWord)) {
                         //UUID関数の呼び出しを見つけたら、そのノードを文字列に変換して保存
                         const code: string = fileContent.substring(node.start, node.end);
                         codes.push(code);
-                    } else if (node.callee.type === 'MemberExpression') {
-                        if (node.callee.object && node.callee.object.type === 'Identifier' && node.callee.object.name.includes(libName)) {
-                            const code: string = fileContent.substring(node.start, node.end);
-                            codes.push(code);
-                        }
+                    } else if(node.callee.type === 'MemberExpression'&&node.callee.object.type === 'Identifier'&& node.callee.object.name.includes(searchWord)){
+                        const code: string = fileContent.substring(node.start, node.end);
+                        codes.push(code);
                     }
                 }
             });
